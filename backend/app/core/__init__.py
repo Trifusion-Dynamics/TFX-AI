631acd1e7de1d807ea6eab7b20ebbc883294c6b3
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-05-03 21:13:03 +0530

    feat(backend): add core init

---
+++ backend/app/core/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core application components.
+"""
+
+from . import config, security, dependencies, exceptions
+
+__all__ = [
+    "config",
+    "security", 
+    "dependencies",
+    "exceptions"
+]

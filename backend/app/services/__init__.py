4116e7faef56ac8e649fe24a9515cc610138da1c
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-05-03 21:16:53 +0530

    feat(backend): add services init

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,19 @@
+"""
+Business logic services package.
+"""
+
+from . import auth_service, user_service, contact_service, service_service, project_service, blog_service, newsletter_service, testimonial_service, pricing_service, case_study_service, ai_tools_service
+
+__all__ = [
+    "auth_service",
+    "user_service",
+    "contact_service",
+    "service_service", 
+    "project_service",
+    "blog_service",
+    "newsletter_service",
+    "testimonial_service",
+    "pricing_service",
+    "case_study_service",
+    "ai_tools_service"
+]
